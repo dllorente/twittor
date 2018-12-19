dac5f076aaf010da171dc5a91c7b8d4bc4e2cd61
David Llorente <davicete11@hotmail.com>
2018-12-19 21:08:29 +0100

    primercommit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+function actualizaCacheDinamico(dynamic_cache,req,res){
+  if(res.ok){
+    return caches.open(dynamic_cache).then(cache=>{
+      cache.put(req,res.clone());
+      return res.clone();
+    });
+  }
+  else {
+    return res;
+  }
+}
