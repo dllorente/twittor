1fa5b125a5ef9e5ecd06f139f1a57895832017dc
David Llorente <davicete11@hotmail.com>
2018-12-26 20:27:58 +0100

    modificaciones para ios

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ var swLocation='/twittor/sw.js';
 if (navigator.serviceWorker)
 {
 
-  if (url.includes('localhost')){
+  if (url.includes('localhost'))
+  {
     swLocation='/sw.js';
   }
   navigator.serviceWorker.register(swLocation);
